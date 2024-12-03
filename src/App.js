58a0b351d17dc91e18b16ce4055247141eb7aa4a
won395 <jeongwon395@gmail.com>
2024-12-03 13:44:53 +0900

    Feat: akkkk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import Calc1 from './comp/calc/study01'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <h1>about</h1>
+        <Routes>
+          <Route path={"/"} element={<Home/>}/>
+          <Route path={"/about"} element={<About/>}/>
+          <Route path={"/cal1"} element={<Calc1/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+}
+
+function About() {
+  return(
+    <div style={{border: '2px blue solid'}}>
+      <Link to="/">Home으로 이동</Link>
+    </div>
+  )
+}
+
+function Home() {
+  return(
+    <div>
+      <h1>Start</h1>
+      <Link to="/about">About으로 이동</Link>
+      <Link to="/cal1">cal1로 이동</Link>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/comp/calc/study01.js
@@ -0,0 +1,7 @@
+export default function Study() {
+    return(
+        <div>
+            <h1>Study01</h1>
+        </div>
+    )
+}
