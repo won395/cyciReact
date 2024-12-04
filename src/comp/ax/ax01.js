35780dce282de832ee2ca26bb418e2e06ab2c7d5
won395 <jeongwon395@gmail.com>
2024-12-04 09:45:31 +0900

    Feat: axios get방식 1

--- src/comp/ax/ax01.js
+++ src/comp/ax/ax01.js
@@ -1,7 +1,30 @@
+import axios from 'axios'
+import { useState } from 'react';
+
 export default function Ax1() {
+
+    const [지역, 변경지역] = useState([]);
+
+    function axios01() {
+        console.log('axios get 방식');
+
+        axios.get('http://localhost:8080/api/area/list')
+        .then(res => {
+            console.log(res);
+            if(res.data.code === '200') {
+                변경지역(res.data.data)
+            }
+        })
+
+        //promise 라는 javaScript 기능하고 비슷 // ES6 문법
+
+        
+    }
+
     return (
         <div>
             <h1>Axios 연습</h1>
+            <input type='button' onClick={axios01} value='get방식'/>
         </div>
     )
 }
