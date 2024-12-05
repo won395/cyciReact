14aca1c9bf5d0df86395c9efdfd3395168daadf2
won395 <jeongwon395@gmail.com>
2024-12-05 10:35:07 +0900

    Feat: html 로그인

--- src/comp/pro/Login.js
+++ src/comp/pro/Login.js
@@ -1,21 +1,32 @@
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom"
 
 export default function Study() {
 
     const navigate = useNavigate
 
+    const idRef = useRef('');
+    const pwRef = useRef('');
+    
+    const loginAction = () => {
+
+        const idValue = idRef.current.value;
+        const pwValue = pwRef.current.value;
+        console.log(pwValue);
+    }
+
     return (
         <div>
             <h1>로그인</h1>
-            <input type="text" placeholder="아이디 입력"/><br/>
-            <input type="password" placeholder="패스워드 입력"/><br/>
+            <input type="text" placeholder="아이디 입력" ref={idRef}/><br/>
+            <input type="password" placeholder="패스워드 입력" ref={pwRef}/><br/>
 
-            <input type="button" value='로그인'/>
             <input type="button" value='회원가입' onClick={
                 () => {
                     navigate('/pro1');
                 }
             }/>
+            <input type="button" value='로그인' onClick={loginAction}/>
         </div>
     )
 }
