26686267e860d5aa96f1fcde4217fb5fbf3ac125
won395 <jeongwon395@gmail.com>
2024-12-08 23:25:58 +0900

    Feat: 게시판 상세 유효성 검사 기능 추가

--- src/comp/pro/BoardDetail.js
+++ src/comp/pro/BoardDetail.js
@@ -18,8 +18,15 @@ export default function BoardDetail() {
     const handleRecommend = () => {
         updateGood(idx).then(res => {
             if (res.data.code === '200') {
-                setBoard({ ...board, good: board.good + 1 });
+                setBoard(prevBoard => ({
+                    ...prevBoard,
+                    good: prevBoard.good + 1
+                }));
+            } else {
+                alert('추천 처리에 실패했습니다.');
             }
+        }).catch(() => {
+            alert('추천 요청 중 오류가 발생했습니다.');
         });
     };
 
@@ -37,7 +44,8 @@ export default function BoardDetail() {
             <div>
                 <h1>{board.title}</h1>
                 <p>{board.content}</p>
-                <button onClick={handleRecommend}>추천: {board.good}</button>
+                <p>추천 수: {board.good}</p>
+                <button onClick={handleRecommend}>추천</button>
                 <button onClick={handleDelete}>삭제</button>
                 <Link to={`/board/edit/${idx}`}>수정</Link>
             </div>
