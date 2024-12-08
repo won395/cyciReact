0099761c84bee3e2ba6c3a367e29c8b33cf640fc
won395 <jeongwon395@gmail.com>
2024-12-08 19:13:36 +0900

    Feat: api/Board 변수명 수정 완료

--- src/comp/api/Board.js
+++ src/comp/api/Board.js
@@ -1,24 +1,25 @@
 import api from '../ax/axiosSetting';
 
-export const fetchPosts = (params) => {
-    return api.get('/posts', { params });
-}
+export const fetchBoards = (params) => {
+    return api.get('/list', { params });
+};
 
-export const fetchPostDetail = (id) => {
-    return api.get(`/posts/${id}`);
-}
-export const createPost = (data) => {
-    return api.post('/posts', data);
-}
+export const fetchBoardDetail = (idx) => {
+    return api.get(`/find?boardId=${idx}`);
+};
 
-export const updatePost = (data) => {
-    return api.put(`/posts/${data.id}`, data);
-}
+export const createBoard = (data) => {
+    return api.post('/regist', data);
+};
 
-export const deletePost = (id) => {
-    return api.delete(`/posts/${id}`);
-}
+export const updateBoard = (data) => {
+    return api.post('/modify', data);
+};
 
-export const updateRecommendation = (id) => {
-    return api.post(`/posts/${id}/recommend`);
-}
+export const deleteBoard = (idx) => {
+    return api.post('/remove', { boardId: idx });
+};
+
+export const updateGood = (idx) => {
+    return api.post('/good', { boardId: idx });
+};
