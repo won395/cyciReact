11f47675eb9bec250d124981014afbdf4c861022
won395 <jeongwon395@gmail.com>
2024-12-11 09:33:04 +0900

    Feat: ㅇㅇ

--- src/comp/red/Red01.js
+++ src/comp/red/Red01.js
@@ -1,8 +1,28 @@
-import { useState } from "react"
+import { useReducer, useState } from "react"
+
+//status: 첫번쨰 인자값 (oldStatus ) 호출 되었을때 리듀서에 담겨 있는 값
+//action: reducer를 호출(해당 행위 명칭을 action)이 되었을때 action에 값이 들어온다
+// reducer 함수는 action의 값을 확인해서 status의 값을 변경한다
+function changeTest(status, action) {
+    console.log('status: ', status);
+    console.log(`action: , ${action}`);
+
+    switch(action) {
+        case 'up':
+            return status+1;
+        case 'down':
+            return status-1;
+        case 'reset':
+            return 0;
+    }
+
+    return status;
+}
 
 export default function Red01() {
 
     const [data, setData] = useState(0);
+    const [test, setTest] = useReducer(changeTest, 0);
 
     return (
         <div>
@@ -15,7 +35,21 @@ export default function Red01() {
             <input type="button" value="감소"
             onClick={e=> {
                 setData(data-1)
-            }}/>
+            }}/><br/>
+
+            {test}<br/>
+            <input type="button" value="증가"
+            onClick={e=> {
+                setTest('up');
+            }}/><br/>
+            <input type="button" value="감소"
+            onClick={e=> {
+                setTest('down');
+            }}/><br/>
+             <input type="button" value="reset"
+            onClick={e=> {
+                setTest('reset');
+            }}/><br/>
         </div>
     )
 
